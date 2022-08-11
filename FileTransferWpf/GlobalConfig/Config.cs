07aacea5c83a6f57283ea093a0f4f7724db92860
CaoDenk <39513475+CaoDenk@users.noreply.github.com>
2022-08-12 00:18:21 +0800

    漏了一个文件

--- FileTransferWpf/GlobalConfig/Config.cs
+++ FileTransferWpf/GlobalConfig/Config.cs
@@ -12,7 +12,7 @@ namespace FileTransferWpf.GlobalConfig
     internal class Config
     {
         public const int TEXT_BUFER_SIZE=1024;
-        public const int FILE_BUFFER_SIZE=1024*256;
+        public const int FILE_BUFFER_SIZE=1024*32;
         public const int OFFSET = 16;
         public const int FULL_SIZE = FILE_BUFFER_SIZE - OFFSET-4;
     }
